8b5d1c52e9f9f543af0db4f2b02f56ed72815fb2
adamki <adajensen@gmail.com>
2016-02-09 03:05:23 -0700

    Iteration 1 - refactored and Iteration 2 - completedI used the .tr method. check this out. it is sweet and will save you a ton of time. once you understand tr, you can dominate strings. I also refact'rd intials.rb

--- initials.rb
+++ initials.rb
@@ -1,5 +1,20 @@
-def initials
-  puts ARGV[0][0] + ARGV[1][0] + ARGV[2][0]
+require 'pry'
+
+class CodeBreaker
+  attr_accessor :words
+
+  def initialize(word_one, word_two, word_three)
+    @words = [word_one, word_two, word_three]
+  end
+
+  def get_first_initial
+    @words.each do |word|
+      print word[0].upcase if word
+    end
+  end
+  
 end
 
-initials
+
+code_breaker = CodeBreaker.new(ARGV[0], ARGV[1], ARGV[2])
+code_breaker.get_first_initial
